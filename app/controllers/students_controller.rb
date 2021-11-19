d87b16622e0c0a7464c500de386a2ea69e656f47
Kenneth Fitzgerald <fitzgeraldkd@gmail.com>
2021-11-19 14:41:15 -0500

    commit tree b1db9a4339581284825260395491a05390bdab93

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,14 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
+    students = Student.all.filter do |student| 
+      student.to_s.match? /#{params[:name]}/i
+    end
     render json: students
+  end
+
+  def show
+    render json: Student.find(params[:id])
   end
 
 end
